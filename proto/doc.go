bcef7be9f56b22ea3ee0b0a0f7f958ff14304020
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2022-05-18 11:40:27 +0200

    2022-05-18 11:40:27

--- proto/doc.go
+++ proto/doc.go
@@ -3,6 +3,7 @@
 /**
  * gRPC codec for the communication toward the hub.
  */
+package proto
 
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative wiy.proto
 
