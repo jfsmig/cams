950911a6f795d8e0e7ef72ca65de47bfc0db9d37
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2023-11-14 13:15:39 +0100

    WIP relicense

--- go/cams-agent/nic.go
+++ go/cams-agent/nic.go
@@ -1,10 +1,23 @@
-// Copyright (c) 2022-2022 Jean-Francois SMIGIELSKI
+// Copyright (c) 2022-2024 The authors (see the AUTHORS file)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
 import (
 	"context"
-	
+
 	"github.com/jfsmig/cams/go/utils"
 	"github.com/jfsmig/onvif/networking"
 	wsdiscovery "github.com/jfsmig/onvif/ws-discovery"
